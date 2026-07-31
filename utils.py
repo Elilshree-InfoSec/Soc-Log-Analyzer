b119ed69b47df3e2b9e9bb182200e948fa8f38e2
Elilshree <25006163@siswa.um.edu.my>
2026-07-31 11:52:01 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+# utility module - contains helper functions
+# Instead of writing the same code over and over, you write it once and reuse it.
+
 def print_title(title):
     print("\n" + "=" * 40)
     print(title)
